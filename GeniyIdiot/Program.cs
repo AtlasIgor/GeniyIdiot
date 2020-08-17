b74d7b32a494d1ebbaa7453d8621f094922444b0
Igor <atlas.ig@ya.ru>
2020-08-17 12:03:23 +0300

    проба изменений

--- GeniyIdiot/Program.cs
+++ GeniyIdiot/Program.cs
@@ -8,62 +8,6 @@ namespace GeniyIdiot
 {
     class Program
     {
-        static string[] GetQuestions(int numberQuestions)
-        {
-            string[] questions = new string[numberQuestions];
-            questions[0] = "Сколько будет два плюс два умноженное на два?";
-            questions[1] = "Бревно нужно распилить на 10 частей, сколько надо сделать  распилов?";
-            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-            questions[3] = "Укол делают каждые полчаса, сколько нужно минут для трех  уколов?";
-            questions[4] = "Пять свечей горело, две потухли. Сколько свечей  осталось?";
-            questions[5] = "На яблоне висело 8 яблок, половину сьели пионеры, сколько яблок осталось на березе?";
-            questions[6] = "Какой сегодня день месяца?";
-            questions[7] = "Какой сегодня номер месяца в году?";
-            questions[8] = "Какой сегодня год?";
-            questions[9] = "Сколько будет дважды два и умножить на два?";
-            questions[10] = "2 в 3 степени?";
-            questions[11] = "2 в 9 степени?";
-
-            return questions;
-        }
-
-        static int[] GetAnswers(int numberQuestions)
-        {
-            int[] answers = new int[numberQuestions];
-            answers[0] = 6;
-            answers[1] = 9;
-            answers[2] = 25;
-            answers[3] = 60;
-            answers[4] = 2;
-            answers[5] = 0;
-            Console.WriteLine("учтите, сегодня: " + DateTime.Today.Day + "." + DateTime.Today.Month + "." + DateTime.Today.Year);
-            answers[6] = DateTime.Today.Day;
-            answers[7] = DateTime.Today.Month;
-            answers[8] = DateTime.Today.Year;
-            answers[9] = 8;
-            answers[10] = 8;
-            answers[11] = 512;
-            return answers;
-        }
-
-        static string[] GetStatus(int numberStatus)
-        {
-            string[] status = new string[6];
-            status[5] = "Гений";
-            status[4] = "Талант";
-            status[3] = "Нормальный";
-            status[2] = "Дурак";
-            status[1] = "Кретин";
-            status[0] = "Идиот";
-            return status;
-        }
-
-        static int[] GetStatusAsk(int numberQuestions)
-        {
-            int[] statusAsk = new int[numberQuestions];
-            for (int i = 0; i < numberQuestions; i++) statusAsk[i] = 1;
-            return statusAsk;    // init array 1 - not use this ask / 0 - use this ask
-        }
 
         static int GetRandomSelect(int[] statusAsk, int numberQuestions)
         {
@@ -77,16 +21,15 @@ namespace GeniyIdiot
             return randomIndex;
         }
 
-        static int PutAsk_GetAnswer(int i, string[] questions, int randomIndex)
+        static int CheckRigthNumerInput(int i, string[] questions, int randomIndex)
         {
-            bool incorrectTryParse = false;
+            bool correctTryParse = false;  // success convert default - false
             int userAnswer = 0;
 
-            while (!incorrectTryParse)
+            while (!correctTryParse)    // while Not correct
             {
-                Console.Write("Вопрос № " + (i + 1) + ".\n" + questions[randomIndex] + " ");
-                incorrectTryParse = int.TryParse(Console.ReadLine(), out userAnswer);
-                if (!incorrectTryParse) Console.WriteLine("\n\tПожалуйста, введите целое число! (последовательность цифр от 0 до 9)\n");
+                correctTryParse = int.TryParse(Console.ReadLine(), out userAnswer);
+                if (!correctTryParse) Console.WriteLine("\n\tПожалуйста, введите целое число! (последовательность цифр от 0 до 9)\n");
             }
             return userAnswer;
         }
@@ -106,7 +49,7 @@ namespace GeniyIdiot
             return -1;
         }
 
-        static string GetFIO()
+        static string GetFIO()    // get name (Russian format)
         {
             string FIO;
             Console.WriteLine("Здравствуйте,\n я программа тестирования на статус от \"Гений\" до \"Идиот\"");
@@ -120,10 +63,10 @@ namespace GeniyIdiot
         {
             int numberQuestions  = 12;
             int numberStatus     = 6;
-            string[] questions   = GetQuestions(numberQuestions);
-            int[]    answers     = GetAnswers  (numberQuestions);
-            int[]    statusAsk   = GetStatusAsk(numberQuestions);
-            string[] statusBrain = GetStatus   (numberStatus);
+            string[] questions   = CreateArrayQuestions(numberQuestions);
+            int[]    arrayAnswers     = CreateArrayAnswers  (numberQuestions);
+            int[]    statusAsk   = CreateArrayStatusQuery(numberQuestions);
+            string[] statusBrain = CreateArrayStatus   (numberStatus);
             int countRightAnswers = 0;
             int randomIndex;
             int userAnswer = 0;
@@ -137,8 +80,9 @@ namespace GeniyIdiot
                 for (int i = 0; i < numberQuestions; i++)
                 {
                     randomIndex = GetRandomSelect(statusAsk, numberQuestions);
-                    userAnswer = PutAsk_GetAnswer(i, questions, randomIndex);
-                    if (userAnswer == answers[randomIndex]) countRightAnswers++;
+                    Console.Write("Вопрос № " + (i + 1) + ".\n" + questions[randomIndex] + " ");  // put Ask
+                    userAnswer = CheckRigthNumerInput(i, questions, randomIndex);
+                    if (userAnswer == arrayAnswers[randomIndex]) countRightAnswers++;
                 }
 
                 percentsOfQualityAnswers = (double)countRightAnswers / numberQuestions;
@@ -149,13 +93,75 @@ namespace GeniyIdiot
                 Console.WriteLine(percentsOfQualityAnswers * 100 + "%\n" + FIO + ", Ваш диагноз: " + statusBrain[numberDiagnos]);
                 Console.Write("Есть жклание повторить? (N - Отказаться / в любом другом случае продолжить):");
                 string consoleRead = Console.ReadLine();
-                if (consoleRead == "N") repeatTest = false;
+                if (consoleRead == "N" || consoleRead == "n" || consoleRead == "Т" || consoleRead == "т") repeatTest = false;
+                                // No,                    no                   Т || т (rus keyboard)
 
-                statusAsk = GetStatusAsk(numberQuestions);
-                statusBrain = GetStatus(numberStatus);
+                // initial for next repeat
+                statusAsk = CreateArrayStatusQuery(numberQuestions);
+                statusBrain = CreateArrayStatus(numberStatus);
+                questions = CreateArrayQuestions(numberQuestions);
+                arrayAnswers = CreateArrayAnswers(numberQuestions);
                 countRightAnswers = 0;
 
             }
         }
     }
+}
+
+static string[] CreateArrayQuestions(int numberQuestions)
+{
+    string[] questions = new string[numberQuestions];
+    questions[0] = "Сколько будет два плюс два умноженное на два?";
+    questions[1] = "Бревно нужно распилить на 10 частей, сколько надо сделать  распилов?";
+    questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+    questions[3] = "Укол делают каждые полчаса, сколько нужно минут для трех  уколов?";
+    questions[4] = "Пять свечей горело, две потухли. Сколько свечей  осталось?";
+    questions[5] = "На яблоне висело 8 яблок, половину сьели пионеры, сколько яблок осталось на березе?";
+    questions[6] = "учтите, сегодня: " + DateTime.Today.Day + "." + DateTime.Today.Month + "." + DateTime.Today.Year + "\nКакой сегодня день месяца?";
+    questions[7] = "учтите, сегодня: " + DateTime.Today.Day + "." + DateTime.Today.Month + "." + DateTime.Today.Year + "\nКакой сегодня номер месяца в году?";
+    questions[8] = "учтите, сегодня: " + DateTime.Today.Day + "." + DateTime.Today.Month + "." + DateTime.Today.Year + "\nКакой сегодня год?";
+    questions[9] = "Сколько будет дважды два и умножить на два?";
+    questions[10] = "2 в 3 степени?";
+    questions[11] = "2 в 9 степени?";
+
+        /* Проверка фиксации изменений на гитхаб */
+
+    return questions;
+}
+
+static int[] CreateArrayStatusQuery(int numberQuestions)
+{
+    int[] statusAsk = new int[numberQuestions];
+    for (int i = 0; i < numberQuestions; i++) statusAsk[i] = 1;
+    return statusAsk;    // init array 1 - not use this ask / 0 - use this ask
+}
+
+static int[] CreateArrayAnswers(int numberQuestions)
+{
+    int[] arrayAnswers = new int[numberQuestions];
+    arrayAnswers[0] = 6;
+    arrayAnswers[1] = 9;
+    arrayAnswers[2] = 25;
+    arrayAnswers[3] = 60;
+    arrayAnswers[4] = 2;
+    arrayAnswers[5] = 0;
+    arrayAnswers[6] = DateTime.Today.Day;
+    arrayAnswers[7] = DateTime.Today.Month;
+    arrayAnswers[8] = DateTime.Today.Year;
+    arrayAnswers[9] = 8;
+    arrayAnswers[10] = 8;
+    arrayAnswers[11] = 512;
+    return arrayAnswers;
+}
+
+static string[] CreateArrayStatus(int numberStatus)
+{
+    string[] status = new string[6];
+    status[5] = "Гений";
+    status[4] = "Талант";
+    status[3] = "Нормальный";
+    status[2] = "Дурак";
+    status[1] = "Кретин";
+    status[0] = "Идиот";
+    return status;
 }
